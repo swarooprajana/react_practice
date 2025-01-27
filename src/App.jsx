88c262b430eb249b5fa808bf680623d3514cfcf4
swarooprajana <kirankalyan159@gmail.com>
2025-01-27 11:05:32 +0530

    adding react code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.scss';
+import Login from './components/Login';
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
